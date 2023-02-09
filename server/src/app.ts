fd9fa50299fbfe74de16e75cc6477f8723815117
vikasrathod4299 <vikas.rv4299@gmail.com>
2023-02-09 12:56:27 +0530

    initail commit

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
